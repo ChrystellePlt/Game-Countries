8173c95c79ccf72890c2addf186b23345b1f4278
Chrystelle Poulet <chrystelle.poulet@hetic.net>
2018-03-04 18:05:08 +0100

    shuffle display corrected

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
 const startButton = document.querySelector('.start-button');
 const gameStart = document.querySelector('#game-start');
 
-startButton.addEventListener('click', function(event){
+startButton.addEventListener('click', function(event) {
   gameStart.classList.remove('is-open');
 })
 
-// display flagscrandomly
+const score = document.querySelector('.score');
+let scoreValue = document.querySelector('.score strong').textContent;
+
+// display flags randomly
 
 let countryName = document.querySelector('.country-name');
-let flags = document.querySelectorAll('.flag');
-let randomFlags = [];
-
-flags.forEach(function(flag) {
-  let randomNumber = flagsData[Math.floor(Math.random() * (flagsData.length + 1))];
-  if (randomFlags.length === 0) {
-    randomFlags.push(randomNumber);
-  } else {
-    if (!randomFlags.includes(randomNumber)) {
-        randomFlags.push(randomNumber);
-    }
-  }
-})
 
 const flagsImage = document.querySelectorAll('.flag > img');
 
 displayFlags();
 
 function displayFlags() {
+  let flags = document.querySelectorAll('.flag');
+  let randomFlags = [];
+
+  while (randomFlags.length != flags.length) { // create an array of four random numbers between from 0 to 103 (which is the number of flags)
+    let randomNumber = flagsData[Math.floor(Math.random()*(flagsData.length + 1))];
+    if (!randomFlags.includes(randomNumber)) {
+        randomFlags.push(randomNumber);
+      }
+  }
+  console.log(randomFlags);
+
   let index = 0;
   flagsImage.forEach(function(image) {
+    console.log(index);
     let countryCode = (randomFlags[index].code).toLowerCase();
     console.log(countryCode);
     image.setAttribute('src', `flags/${countryCode}.svg`);
     image.setAttribute('name', `${randomFlags[index].name}`);
     index++;
   })
-  countryName.innerHTML = randomFlags[Math.floor(Math.random() * (randomFlags.length + 1))].name;
+  countryName.innerHTML = randomFlags[Math.floor(Math.random()*(randomFlags.length))].name;
 };
 
+
+// add eventListener on flags + add +1 to score if match
 flagsImage.forEach(function(image) {
-image.addEventListener('click', function(event){
-  let targetNameAttribute = event.target.getAttribute('name');
-  if (targetNameAttribute === countryName.textContent) {
-    alert('congrats')
-  }
-})
+  image.addEventListener('click', function(event) {
+    let targetNameAttribute = event.target.getAttribute('name');
+    if (targetNameAttribute === countryName.textContent) {
+      scoreValue++;
+      score.innerHTML = 'Your score : <strong>' + scoreValue + '</strong>';
+      displayFlags();
+    }
+  })
 })
