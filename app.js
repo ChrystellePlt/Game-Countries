e331e2f4ffe7d727b823cd2c16191d8b3f163ad2
Chrystelle Poulet <chrystelle.poulet@hetic.net>
2018-03-05 21:53:46 +0100

    lives counter added + game over after 3 mistakes or when countdown = 0sec

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const startButton = document.querySelector('.start-button');
 const restartButton = document.querySelector('.restart-button');
 const gameStart = document.querySelector('#game-start');
 const gameOver = document.querySelector('#game-over');
-var timeLeft = 20;
 
 startButton.addEventListener('click', function(event) {
   gameStart.classList.remove('is-open');
-  var timer = document.querySelector('.time');
+  game();
+})
 
+restartButton.addEventListener('click', function(event) {
+  gameOver.classList.remove('is-open');
+  location.reload();
+})
+
+function game() {
+  var timeLeft = 20;
+  var timer = document.querySelector('.time');
   var timerId = setInterval(countdown, 1000);
 
   function countdown() {
@@ -19,68 +27,74 @@ startButton.addEventListener('click', function(event) {
       timeLeft--;
     }
   }
-})
+  let score = document.querySelector('.score');
+  let scoreValue = 0;
 
-const score = document.querySelector('.score');
-let scoreValue = document.querySelector('.score strong').textContent;
+  // display flags randomly
 
-// display flags randomly
+  let countryName = document.querySelector('.country-name');
 
-let countryName = document.querySelector('.country-name');
+  const flagsImage = document.querySelectorAll('.flag > img');
 
-const flagsImage = document.querySelectorAll('.flag > img');
+  displayFlags();
 
-displayFlags();
+  function displayFlags() {
+    let flags = document.querySelectorAll('.flag');
+    let randomFlags = [];
 
-function displayFlags() {
-  let flags = document.querySelectorAll('.flag');
-  let randomFlags = [];
-
-  while (randomFlags.length != flags.length) { // create an array of four random numbers between from 0 to 103 (which is the number of flags)
-    let randomNumber = flagsData[Math.floor(Math.random()*(flagsData.length + 1))];
-    if (!randomFlags.includes(randomNumber)) {
+    while (randomFlags.length != flags.length) { // create an array of four random numbers between from 0 to 103 (which is the number of flags)
+      let randomNumber = flagsData[Math.floor(Math.random() * (flagsData.length + 1))];
+      if (!randomFlags.includes(randomNumber)) {
         randomFlags.push(randomNumber);
       }
-  }
-
-  let index = 0;
-  flags.forEach(function(flag){
-    if (flag.classList.contains('is-active')) {
-     flag.classList.remove('is-active');
-}
-  })
-  flagsImage.forEach(function(image) {
-    let countryCode = (randomFlags[index].code).toLowerCase();
-    image.setAttribute('src', `flags/${countryCode}.svg`);
-    image.setAttribute('name', `${randomFlags[index].name}`);
-    index++;
-  })
-  countryName.innerHTML = randomFlags[Math.floor(Math.random()*(randomFlags.length))].name;
-};
-
-let lives = document.querySelectorAll('.lives > img');
-let counter = 0;
-// add eventListener on flags + add +1 to score if match
-flagsImage.forEach(function(image) {
-  image.addEventListener('click', function(event) {
-    let imageAttribute = image.getAttribute('name');
-    if (imageAttribute === countryName.textContent) {
-      scoreValue++;
-      if (timeLeft < 28) {
-        timeLeft += 3;
-      } else {
-        timeLeft += 30 - timeLeft;
-      }
-      score.innerHTML = 'Your score : <strong>' + scoreValue + '</strong>';
-      displayFlags();
-    } else {
-      let parent = event.target.parentElement;
-      parent.classList.add('is-active');
-      lives[counter].classList.add('is-active');
-      counter++;
-      if (counter === 3) {
-        gameOver.classList.add('is-open');
-      }
     }
+
+    let flagsPlayed = [];
+
+    let index = 0;
+    flags.forEach(function(flag) {
+      if (flag.classList.contains('is-active')) {
+        flag.classList.remove('is-active');
+      }
+    })
+    flagsImage.forEach(function(image) {
+      let countryCode = (randomFlags[index].code).toLowerCase();
+      image.setAttribute('src', `flags/${countryCode}.svg`);
+      image.setAttribute('name', `${randomFlags[index].name}`);
+      index++;
+    })
+    countryName.innerHTML = randomFlags[Math.floor(Math.random() * (randomFlags.length))].name;
+    let flagPlayed = countryName.textContent;
+    console.log(flagPlayed);
+    flagsPlayed.push(flagPlayed);
+    console.log(flagsPlayed);
+  };
+
+  let lives = document.querySelectorAll('.lives > img');
+  let counter = 0;
+  // add eventListener on flags + add +1 to score if match
+  flagsImage.forEach(function(image) {
+    image.addEventListener('click', function(event) {
+      let imageAttribute = image.getAttribute('name');
+      if (imageAttribute === countryName.textContent) {
+        scoreValue++;
+        if (timeLeft < 28) {
+          timeLeft += 3;
+        } else {
+          timeLeft += 30 - timeLeft;
+        }
+        score.innerHTML = 'Your score : <strong>' + scoreValue + '</strong>';
+        displayFlags();
+      } else {
+        let parent = event.target.parentElement;
+        parent.classList.add('is-active');
+        lives[counter].classList.add('is-active');
+        counter++;
+        if (counter === 3) {
+          clearTimeout(timerId);
+          gameOver.classList.add('is-open');
+        }
+      }
+    })
   })
-})
+}
